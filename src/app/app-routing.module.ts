3d6cad9570bea9637f730b18b9ed28904bcb2bb3
Terwase Gberikon <7498753+tekrondo@users.noreply.github.com>
2020-03-03 09:25:35 +0100

    Created the TaskService and the WebRequestService to consume the API.
    Created a new route for the new-list button so content can be sent to the backend
    Basic app functionalit in barebones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
+import { NewListComponent } from './pages/new-list/new-list.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: TaskViewComponent
+  },
+  {
+    path: 'new-list',
+    component: NewListComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
+import { NewListComponent } from './pages/new-list/new-list.component';
+// import { WebRequestService } from './services/web-request.service';
+// import { TaskService } from './services/task.service'
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskViewComponent
+    TaskViewComponent,
+    NewListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // WebRequestService,
+    // TaskService,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
